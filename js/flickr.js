45237ec506c558c2afd912e964b1690ea946bba0
Nicole Archambault <ABS01@W33111-ABS-ANNX.labs.mhcc.cc.or.us>
2015-08-06 12:50:04 -0700

    Inital commit

---
+++ js/flickr.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  
+  // Change button appearance on click
+ var $button = $('button');
+      $button.click(function() {
+      $button.removeClass("selected");
+      $(this).addClass("selected");
+    
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+      tags: animal,
+      format: 'json'
+    };
+    
+    $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+    
+}); // end button click function
+  
+  function displayPhotos ( data ) {
+      var photoHTML = '<ul>';
+      $.each( data.items, function (i, photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
+      
+      photoHTML+= '</ul>';
+      $('#photos').html(photoHTML);
+   };
+    
+}); // end ready
